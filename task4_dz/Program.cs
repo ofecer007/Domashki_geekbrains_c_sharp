74b09c303917683e5d31afcc81267add41a1069a
Vladimi Anufriev <vova24@yandex.ru>
2022-09-30 01:05:33 +0300

    test 123

--- task4_dz/Program.cs
+++ task4_dz/Program.cs
@@ -9,11 +9,10 @@ Console.WriteLine("Введите первое число:");
 int userNumber1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int userNumber2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число:");
+Console.WriteLine("Введите третье число:");
 int userNumber3 = Convert.ToInt32(Console.ReadLine());
 //Алгоритм
 if(userNumber1 < userNumber2)
     Console.WriteLine ($"Число {userNumber2} больше числа {userNumber1}");
 else
     Console.WriteLine ($"Число {userNumber1} больше числа {userNumber2}");
-    
