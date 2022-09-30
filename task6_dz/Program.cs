4fa5ddc7366317933cf29120348a50e388045edb
Vladimi Anufriev <vova24@yandex.ru>
2022-09-30 09:56:25 +0300

    Сделал всю домашку

---
+++ task6_dz/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+//Ввод данных
+Console.WriteLine("Программа напишет является ли число чётным");
+Console.WriteLine("Введите число:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+//Алгоритм
+if(userNumber %2==0) 
+    Console.WriteLine ($"{userNumber} четное");
+else 
+    Console.WriteLine ($"{userNumber} нечетное");
---
+++ task8_dz/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+//Ввод данных
+Console.WriteLine("Программа напишет все четные числа от 2 до введеного пользователем числа");
+Console.WriteLine("Введите число:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int whileNumber;
+//Алгоритм
+whileNumber = 2;
+while (userNumber >= whileNumber)
+{
+    Console.WriteLine ($"{whileNumber}");
+    whileNumber = whileNumber + 2;
+}
