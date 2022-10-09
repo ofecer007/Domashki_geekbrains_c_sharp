2b52f3bcea856360e35e96159d8016264608ba76
Vladimi Anufriev <vova24@yandex.ru>
2022-10-09 19:50:42 +0300

    Сделал 10/13/15 домшки

--- task13_dz/Program.cs
+++ task13_dz/Program.cs
@@ -6,20 +6,20 @@
 32679 -> 6
 */
 
+//Ввод данных
 Console.Clear();
-Console.WriteLine("Программа напишет вторую цифру 3х значного числа, которое введет пользователь");
+Console.WriteLine("Программа напишет третью цифру 3х значного числа, которое введет пользователь");
 Console.WriteLine("Введите 3х значное число:");
 int userNumber = Convert.ToInt32(Console.ReadLine());
 //Алгоритм
-int Result = 0;
-if (userNumber > 100 && userNumber < 999)
-{   
-    Result = userNumber/10;
-    while(Result > 10)
-        {
-            Result = Result-10;
-        }
-    Console.WriteLine($"ответ = {Result}");
+if (userNumber >= 100)
+{
+    while(userNumber > 1000)
+    {
+        userNumber/=10;
+    }
+    userNumber%=10;
+    Console.WriteLine($"ответ = {userNumber}");
 }
 else
-Console.WriteLine($"Третей цифры нет!");
+    Console.WriteLine($"Третей цифры нет!");
--- task15_dz/Program.cs
+++ task15_dz/Program.cs
@@ -5,3 +5,17 @@
 7 -> да
 1 -> нет
 */
+
+//Ввод данных
+Console.Clear();
+Console.WriteLine("Программа напишет является ли день недели введеный пользователем выходным");
+Console.WriteLine("Введите день недели числом (1-7):");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+//Алгоритм
+if (userNumber >= 1 && userNumber <= 7)
+    if (userNumber >= 1 && userNumber <= 5)
+    Console.WriteLine($"{userNumber} рабочий");
+else
+    Console.WriteLine($"{userNumber} выходной");
+if (userNumber < 1 && userNumber > 7)
+    Console.WriteLine($"Ошибка!");
