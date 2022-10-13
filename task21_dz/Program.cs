44572076c3ca042a3171d32df3c4a3bbe234647f
Vladimi Anufriev <vova24@yandex.ru>
2022-10-13 19:59:48 +0300

    Доделал 21ую домашку

--- task21_dz/Program.cs
+++ task21_dz/Program.cs
@@ -1,25 +1,5 @@
-﻿/*
-Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-A (3,6,8); B (2,1,-7), -> 15.84
-A (7,-5, 0); B (1,-1,9) -> 11.53                                   */
-
-//Ввод данных
-Console.Clear();
-Console.WriteLine("Программа напишет растояние между 2мя точками в 3D пространстве");
-Console.Write("Введите кардинату X точки А: ");
-int userCoordinateX1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кардинату Y точки А: ");
-int userCoordinateY1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кардинату Z точки А: ");
-int userCoordinateZ1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кардинату X точки B: ");
-int userCoordinateX2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кардинату Y точки B: ");
-int userCoordinateX2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите кардинату Z точки B: ");
-int userCoordinateX2 = Convert.ToInt32(Console.ReadLine());
-
-//Алгоритм
+﻿/* Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+*/
 
 int getCoordinateFromUser(string userInformation)
 {
@@ -27,19 +7,21 @@ int getCoordinateFromUser(string userInformation)
     Console.Write($"{userInformation} ");
     while (!int.TryParse(Console.ReadLine(), out result))
     {
-        Console.Write($"Ошибка ввода! Ожидается целое число. {userInformation} ");
+        Console.Write($"Ошибка ввода! Ожидается целое число. {userInformation}: ");
     }
     return result;
 }
+
 int userCoordinateX1 = getCoordinateFromUser("X1 = ");
 int userCoordinateY1 = getCoordinateFromUser("Y1 = ");
 int userCoordinateZ1 = getCoordinateFromUser("Z1 = ");
 int userCoordinateX2 = getCoordinateFromUser("X2 = ");
 int userCoordinateY2 = getCoordinateFromUser("Y2 = ");
 int userCoordinateZ2 = getCoordinateFromUser("Z2 = ");
-double findRangeBetweenTwoPoints(int coordinateX1, int coordinateY1,int coordinateZ1, int coordinateX2, int coordinateY2, int coordinateZ2)
+
+double findRangeBetweenTwoPoints(int coordinateX1, int coordinateY1, int coordinateZ1, int coordinateX2, int coordinateY2, int coordinateZ2 )
 {
-    return Math.Sqrt(Math.Pow(coordinateX2 - coordinateX1, 2) + Math.Pow(coordinateY2 - coordinateY1, 2));
+    return Math.Sqrt(Math.Pow(coordinateX2 - coordinateX1, 2) + Math.Pow(coordinateY2 - coordinateY1, 2) + Math.Pow(coordinateZ2 - coordinateZ1, 2));
 }
 
-Console.WriteLine(findRangeBetweenTwoPoints(userCoordinateX1, userCoordinateY1, userCoordinateX2, userCoordinateY2));
+Console.WriteLine(findRangeBetweenTwoPoints(userCoordinateX1, userCoordinateY1, userCoordinateZ1, userCoordinateX2, userCoordinateY2, userCoordinateZ2 ));
