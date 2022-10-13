90ba0a9ce756a1cdfeae08e6b4d70864d2d0a9c0
Vladimi Anufriev <vova24@yandex.ru>
2022-10-13 18:16:03 +0300

    Сделана 19 и 23 домшки

---
+++ task19_dz/Program.cs
@@ -0,0 +1,27 @@
+﻿/*  Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да                  */
+
+//Ввод данных
+Console.Clear();
+Console.WriteLine("Программа напишет, является число введенное пользователем палиндромом (12821, 23432)");
+Console.WriteLine("Введите число");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+//Алгоритм
+int reversUserNumber(int userNumber)  // Кто-то улучшил эту часть кода
+{
+    int reverseNumber = 0;
+    while(userNumber > 0)
+    {
+        reverseNumber *= 10;
+        reverseNumber += userNumber % 10;
+        userNumber /= 10;
+    }  
+    return reverseNumber;
+}
+if(reversUserNumber(userNumber) == userNumber) 
+    Console.WriteLine("ДА! Это число палиндром!");
+else Console.WriteLine("НЕТ! Это число не является палиндромом!");
---
+++ task23_dz/Program.cs
@@ -0,0 +1,22 @@
+﻿/* 
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125           */
+
+//Ввод данных
+Console.Clear();
+Console.WriteLine("Программа напишет таблицу кубов чисел от 1 до того числа что введет пользователь");
+Console.Write("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+//Алгоритм
+int task;
+Console.Write("Решение: ");
+for (int i=1; i <= userNumber; i++)
+    {
+        task = i;
+        task = Convert.ToInt32(Math.Pow(i,3));
+        Console.Write($"{task}, ");
+    }
+Console.Write($"конец");
