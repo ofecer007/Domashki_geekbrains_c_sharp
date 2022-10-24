18c145c378c6e077e0b13e1cebbf8463dc09bbc4
Vladimi Anufriev <vova24@yandex.ru>
2022-10-24 18:23:55 +0300

    41+43 сделаны

---
+++ task41_dz/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+int[] fillArray(int arraySize)
+{
+    int[] userArray = new int[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        userArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return userArray;
+}
+
+void printArray(int[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length -1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int countOfNumbersBiggerZero(int[] userArray)
+{
+    int count = 0;
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Сколько чисел будем вводить? ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+int[] userArray = fillArray(arraySize);
+printArray(userArray);
+
+int countNumbers = countOfNumbersBiggerZero(userArray);
+Console.WriteLine($"Количество чисел больше ноля = {countNumbers}");
---
+++ task43_dz/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double userPoint(string msg)
+{
+    Console.Write($"Введите точку {msg}: ");
+    double point = Convert.ToDouble(Console.ReadLine());
+    return point;
+}
+
+double[] crossPoint(double b1, double k1, double b2, double k2)
+{
+    double[] y = new double[2];
+    double x = (b2 - b1) / (k1 - k2);
+    y[0] = k1 * x + b1;
+    y[1] = k2 * x + b2;
+    return y;
+}
+
+void printArray(double[] incomingArray)
+{
+    Console.Write("(");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length -1)
+        {
+            Console.Write("; ");
+        }
+    }
+    Console.WriteLine(")");
+}
+
+Console.WriteLine("Чтобы найти точку пересечения двух прямых, задайте точки координат.");
+
+double b1 = userPoint("b1");
+double k1 = userPoint("k1");
+double b2 = userPoint("b2");
+double k2 = userPoint("k2");
+
+double[] result = crossPoint(b1, k1, b2, k2);
+printArray(result);
